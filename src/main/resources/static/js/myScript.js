4d9c21143d9538cdd1abb7aa5a43b16a21a98b0d
252547545 <LIUfeng520>
2019-11-18 16:55:11 +0800

    加入iframe

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -8,3 +8,10 @@ $('#menu ul li').bind('click',
     $(this).css("background-color","#1d1d1d");
     $(this).children("a").css("border-left-color","#00cdcd");
 });
+
+$('.menu_back a').bind('click',
+    function() {
+        $(window.parent.document).attr("title","云辉相映");
+        $(window.parent.document).find("#newwrap").css("visibility","hidden");
+    });
+
